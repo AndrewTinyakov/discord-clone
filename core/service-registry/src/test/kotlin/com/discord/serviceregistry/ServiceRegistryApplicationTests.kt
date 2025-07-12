a9db8b218095c33a50d90338afa5a1b8ce215d60
AndrewTinyakov <oker.2005@mail.ru>
2025-07-12 14:24:55 +0300

    init config server, gateway, service-registry

---
+++ core/service-registry/src/test/kotlin/com/discord/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.discord.serviceregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "discord-clone"
+include("core")
