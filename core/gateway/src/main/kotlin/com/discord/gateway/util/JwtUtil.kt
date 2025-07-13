4ea9110507ed01513af4451efd963bf894e7a060
AndrewTinyakov <oker.2005@mail.ru>
2025-07-14 02:05:25 +0300

    fixed security filter - removed private key

--- core/gateway/src/main/kotlin/com/discord/gateway/util/JwtUtil.kt
+++ core/gateway/src/main/kotlin/com/discord/gateway/util/JwtUtil.kt
@@ -3,18 +3,16 @@ package com.discord.gateway.util
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import org.springframework.stereotype.Component
-import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
 
 @Component
 class JwtUtil(
-    private val privateKey: RSAPrivateKey,
     private val publicKey: RSAPublicKey
 ) {
 
 
     fun validateToken(token: String?): Pair<String, String> {
-        val valid = JWT.require(Algorithm.RSA256(publicKey, privateKey))
+        val valid = JWT.require(Algorithm.RSA256(publicKey))
             .build()
             .verify(token)
 
