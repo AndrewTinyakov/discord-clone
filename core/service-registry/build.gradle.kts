ed58cff012e7cf166bdebee4216016903d7424a1
imZontik <elvin-nadirov-321@mail.ru>
2025-07-16 21:44:06 +0300

    fix config server and service registry configs

--- core/service-registry/build.gradle.kts
+++ core/service-registry/build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2024.0.1"
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
