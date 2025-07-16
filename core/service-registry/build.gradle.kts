72c87967b1a95454128f31c84983a9376d4884ef
AndrewTinyakov <oker.2005@mail.ru>
2025-07-16 20:15:36 +0300

    made user service insert user idempotent added config to config server for service registry

--- core/service-registry/build.gradle.kts
+++ core/service-registry/build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2024.0.1"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
